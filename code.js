f4d2df5477f71b99c273e2b88cc7041706d52ad7
thaygiaoha <thaygiaotoan6688@gmail.com>
2026-04-09 13:38:44 +0700

    Update code.js

--- code.js
+++ code.js
@@ -274,7 +274,6 @@ const params = e.parameter;
 
   const examCodeInput = (e.parameter.examCode || "").trim();
   const questionIdInput = (e.parameter.questionId || "").trim();
-  const idgv = (e.parameter.idgv || "").trim();
 
   const data = sheet.getDataRange().getValues();
 
@@ -282,9 +281,8 @@ const params = e.parameter;
 
     const rowExam = String(data[i][0]).trim();
     const rowId = String(data[i][1]).trim();
-    const rowIdgv = String(data[i][7]).trim();
 
-    if (rowExam === examCodeInput && rowId === questionIdInput && N9(rowIdgv) === N9(idgv)) {
+    if (rowExam === examCodeInput && rowId === questionIdInput) {
 
   return createResponse(
     "success",

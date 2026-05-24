d370b90d7a24a32d90d75465a47a5a38ca2787b8
thaygiaoha <thaygiaotoan6688@gmail.com>
2026-05-25 01:05:51 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1021,7 +1021,7 @@ if (action === "submitExam" || action === "submitExamMatrix") {
 
         if (
           rowMaDe === maDe &&
-          rowIdgv === idgv
+          N9(rowIdgv) === N9(idgv)
         ) {
 
           foundLink = rowLink;
@@ -1424,7 +1424,7 @@ if (closeTime && now > closeTime) {
     // Lấy list mã đề của Giáo Viên
     // Lấy danh sách toàn bộ mã đề của Giáo viên từ cả 2 sheet
 if (action === "getListMade") {
-  const idgv = (data.idgv || "").toString().trim();
+  const idgv = (N9(data.idgv) || "").toString().trim();
 
   if (!idgv) {
     return resJSON({ status: "error", message: "Vui lòng nhập IDGV!" });
@@ -1439,7 +1439,7 @@ if (action === "getListMade") {
     if (lastRowMatran >= 2) {
       const dataMatran = sheetMatran.getRange(2, 1, lastRowMatran - 1, 2).getValues(); // Cột A (1), B (2)
       for (let i = 0; i < dataMatran.length; i++) {
-        const currentIdgv = (dataMatran[i][0] || "").toString().trim(); // Cột A
+        const currentIdgv = (N9(dataMatran[i][0]) || "").toString().trim(); // Cột A
         const currentMaDe = (dataMatran[i][1] || "").toString().trim(); // Cột B
         
         if (currentIdgv === idgv && currentMaDe) {
@@ -1460,7 +1460,7 @@ if (action === "getListMade") {
       const dataExams = sheetExams.getRange(2, 1, lastRowExams - 1, 16).getValues(); // Lấy từ cột A đến P (16)
       for (let i = 0; i < dataExams.length; i++) {
         const currentMaDe = (dataExams[i][0] || "").toString().trim(); // Cột A
-        const currentIdgv = (dataExams[i][1] || "").toString().trim(); // Cột B
+        const currentIdgv = (N9(dataExams[i][1]) || "").toString().trim(); // Cột B
         const valueCotP = (dataExams[i][15] || "").toString().trim();  // Cột P (index 15)
 
         if (currentIdgv === idgv && currentMaDe) {

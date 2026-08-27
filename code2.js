75450ae6014393d06688a9b1d9d9a2ce6777bee7
thaygiaoha <thaygiaotoan6688@gmail.com>
2026-08-27 12:36:06 +0700

    Update code2.js

--- code2.js
+++ code2.js
@@ -516,8 +516,14 @@ if (type === 'verifyStudent' || action === 'verifyStudent') {
 if (action === 'getLG') {
   try {
     const idTraCuu = supper(params.id || "");
-    if (!idTraCuu) return createResponse("error", "Thiếu ID tra cứu!");
-
+    const passTracuu = String(params.pass || "").trim();
+    const idgvTracuu = String(params.idgv || "").trim();
+    
+    if (!idTraCuu || !passTracuu || !idgvTracuu) return createResponse("error", "Thiếu thông tin tra cứu!");
+    const sheetId = getSheetIdByIdgv(idgvTracuu);
+    const sheetmatran = getSS2Sheet(sheetId, idgvTracuu, "matran");
+    const passL1 = sheetmatran ? sheetmatran.getRange("L1").getValue() : "";
+    if (passTracuu !== String(passL1).trim()) return createResponse("error", "Mật khâu sai rồi bạn yêu! Hãy liên hệ với thầy cô " + idgvTracuu );
     const lastRow = sheetNH.getLastRow();
     if (lastRow < 2) return createResponse("error", "Ngân hàng đang trống!");
 
